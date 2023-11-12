acfa418c076fd6901272af41ea1cddff99fce345
shriya1808 <mittapallishriya@gmail.com>
2023-11-12 16:42:21 +0100

    making pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Shriya Mittapalli",
+    author_email="mittapallishriya@gmail.com",
+    packages=find_packages(),
+    description='setup.py for Math Quiz'
+)
