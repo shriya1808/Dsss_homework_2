7b01a6d53758fcc4690d3d3b141a2d3d009d7217
shriya1808 <mittapallishriya@gmail.com>
2023-11-12 17:07:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="math_quiz",
     version="1.0",
+    url="https://github.com/shriya1808/Dsss_homework_2",
     author="Shriya Mittapalli",
     author_email="mittapallishriya@gmail.com",
     packages=find_packages(),
-    description='setup.py for Math Quiz'
+    description='setup.py for Math Quiz',
 )
