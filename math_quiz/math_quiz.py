1a461d10f87ae0ddf6b3a5fd920ecbae3ef2f422
shriya1808 <mittapallishriya@gmail.com>
2023-11-12 15:17:42 +0100

    cleaning code in math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,84 @@
 import random
 
 
-def function_A(min, max):
+def choose_random_integer(min, max):
     """
-    Random integer.
+    Args:
+        min (int): lower bound
+        max (int): upper bound
+
+    Returns:
+        (int) A Random integer between [min, max], including both points.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def choose_random_operator():
+    """
+    Returns:
+        (str) A Random operator '+', '-' or '*'.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def get_question_and_answer(n1, n2, o):
+    """
+    Args:
+        n1 (int): left operand
+        n1 (int): right operand
+        o (str): operator
+
+    Returns:
+        question (str): the question to be solved by the user.
+        answer (int): answer for the question.
+    """
+    question = f"{n1} {o} {n2}"
+
+    # if-else condition checks for which operation to be performed
+    if o == '+': answer = n1 + n2
+    elif o == '-': answer = n1 - n2
+    else: answer = n1 * n2
+
+    return question, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Conducts the Math Quiz Game by generating 5 questions and awards user 1 point for each correct answer.
+    """
+
+    score = 0  # points scored by the user is set to 0 initially
+    ques = 5    # number of questions in Quiz
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(ques):
+        # choosing random operands and operator
+        n1 = choose_random_integer(1, 10)
+        n2 = choose_random_integer(1, 5)
+        o = choose_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        # get Question and its answer
+        question, answer = get_question_and_answer(n1, n2, o)
 
-        if useranswer == ANSWER:
+        # print question and get user answer
+        print(f"\nQuestion: {question}")
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except:
+            print("Invalid Input, value must be an integer.")
+            print(f"The correct answer is {answer}.")
+            continue
+
+        # check user answer
+        if useranswer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1   # award 1 point to correct answer
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{ques}")
 
 if __name__ == "__main__":
     math_quiz()
