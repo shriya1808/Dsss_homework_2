71a2dbeefc416deb10e7e0d8d574c26f48166564
shriya1808 <mittapallishriya@gmail.com>
2023-11-16 15:40:17 +0100

    update function names

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -4,7 +4,7 @@ from math_quiz import choose_random_integer, choose_random_operator, get_questio
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_choose_random_integer(self):
+    def test_choose_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
@@ -12,14 +12,14 @@ class TestMathGame(unittest.TestCase):
             rand_num = choose_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_choose_random_operator(self):
+    def test_choose_random_operator(self):
         # Test if operator generated is '+', '-' or '*'
         operators = ['+', '-', '*']
         for _ in range(1000):  # Test large number of times
             rand_operator = choose_random_operator()
             self.assertIn(rand_operator, operators)
 
-    def test_function_get_question_and_answer(self):
+    def test_get_question_and_answer(self):
         # Test if questions and answers generated are correct
 
         # test case format (left operand, right operand, operator, expected_problem, expected_answer)
